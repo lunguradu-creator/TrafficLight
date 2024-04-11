a9985b770029a73aca49a5ad1b9111200a4849be
Radu Lungu <149625218+lunguradu-creator@users.noreply.github.com>
2024-04-11 08:44:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+﻿const button = document.getElementById('myButton');
+let colors = ['btn-danger', 'btn-warning', 'btn-success'];
+let currentColorIndex = 0;
+
+// Function to change button color
+function changeColor() {
+    button.classList.remove(colors[currentColorIndex]);
+    currentColorIndex = (currentColorIndex + 1) % colors.length; // Cycle through the colors array
+    button.classList.add(colors[currentColorIndex]);
+}
+
+// Call the changeColor function every 10 seconds
+let intervalId = setInterval(changeColor, 10000);
+
+button.addEventListener('click', function () {
+    clearInterval(intervalId); // Stop the automatic color change
+    changeColor(); // Change color on click
+
+    // Restart the automatic color change after 10 seconds
+    setTimeout(() => {
+        intervalId = setInterval(changeColor, 10000);
+    }, 10000);
+});
